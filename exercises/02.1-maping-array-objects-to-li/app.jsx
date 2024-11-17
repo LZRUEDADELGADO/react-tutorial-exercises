9d7545d99ff4bce0f0eee61c7ea5fe16f835e13e
Lynda <155568879+LZRUEDADELGADO@users.noreply.github.com>
2024-11-17 18:54:47 +0000

    hasta 3.3

--- exercises/02.1-maping-array-objects-to-li/app.jsx
+++ exercises/02.1-maping-array-objects-to-li/app.jsx
@@ -3,11 +3,8 @@ import ReactDOM from "react-dom";
 
 const animals = [{ label: "Horse" }, { label: "Turtle" }, { label: "Elephant" }, { label: "Monkey" }];
 
-/**
- * Change the content of the map function
- **/
 const animalsInHTML = animals.map((singleAnimal, index) => {
-	return <li>hello</li>;
+	return <li key={index}>{singleAnimal.label}</li>;
 });
 
 ReactDOM.render(<ul>{animalsInHTML}</ul>, document.querySelector("#myDiv"));
